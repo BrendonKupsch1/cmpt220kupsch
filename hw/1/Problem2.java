1c004167bc9ea839f8e211398999ff414c1e6160
BrendonKupsch1 <Brendon.Kupsch1@marist.edu>
2022-02-02 17:38:10 -0500

    problem 2 hw 1

---
+++ hw/1/Problem2.java
@@ -0,0 +1,7 @@
+public class Problem2 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("If the starting velocity of an object is 4.6 meters per second, the ending velocity is 11.5 meters per second, and time in seconds is 0.6, then average acceleration is " + ((11.5 - 4.6) / 0.6) + " meters per seconds squared.");
+    }
+}
