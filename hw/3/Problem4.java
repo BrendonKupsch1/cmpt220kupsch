9afa14751a0e8c19e1237289d4e8a6ffa83a643e
BrendonKupsch1 <Brendon.Kupsch1@marist.edu>
2022-02-16 19:28:48 -0500

    hw 3

--- hw/3/Problem4.java
+++ hw/3/Problem4.java
@@ -6,6 +6,10 @@ public class Problem4
 {
     public static void main(String[] args)
     {
-        
+        double point1 = (Math.random() * 5);
+        double point2 = (Math.random() * 5);
+        System.out.println("The point is ("+point1+", "+point2+")");
+        double distance = (Math.pow(((Math.pow(point1, 2)) + (Math.pow(point2, 2))), 0.5));
+        System.out.println("and its distance to the center is "+distance);
     }
 }
