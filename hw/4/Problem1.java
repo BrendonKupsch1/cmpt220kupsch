cd70fc51e94406f097407f55b3d52052a0633871
BrendonKupsch1 <Brendon.Kupsch1@marist.edu>
2022-02-23 18:56:03 -0500

    hw4 prob1

--- hw/4/Problem1.java
+++ hw/4/Problem1.java
@@ -2,7 +2,19 @@
 //CMPT_220L_202
 //Prof. Arias
 //HW 4 Problem 1
+import java.util.Scanner;
 public class Problem1 
 {
-
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of sides of your polygon: ");
+        double sides = input.nextDouble();
+        System.out.println("Enter the length of your sides: ");
+        double length = input.nextDouble();
+        double area = (sides * Math.pow(length, 2)) / (4 * (Math.tan(Math.PI / sides)));
+        System.out.println("The area of the polygon is: " + area + ".");
+        input.close();
+    }
+    
 }
--- hw/4/Problem2.java
+++ hw/4/Problem2.java
@@ -2,6 +2,10 @@
 //CMPT_220L_202
 //Prof. Arias
 //HW 4 Problem 2
-public class Problem2 {
-    
+public class Problem2 
+{
+    public static void main(String[] args)
+    {
+        
+    }
 }
