b2ef4bdfc8a22e01790957f4965ec058bbf082cd
BrendonKupsch1 <Brendon.Kupsch1@marist.edu>
2022-03-02 18:09:42 -0500

    hw5 prob2

--- hw/5/Problem2.java
+++ hw/5/Problem2.java
@@ -5,5 +5,23 @@
 import java.util.Scanner;
 public class Problem2 
 {
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a decimal number to convert it to binary: ");
+        int decimal = input.nextInt();
+        System.out.println(decimalToBinary(decimal));
+    }
     // take % 2, if 0 then even, take % of 2 and divide by 2 till you run out of numbers 
+    public static String decimalToBinary(int value)
+    {
+        String s = "";
+        while (value > 0)
+        {
+            s = ((value % 2) == 0 ? "0" : "1") +s;
+            value = value / 2;
+        }
+        return s;
+    }
 }
+
