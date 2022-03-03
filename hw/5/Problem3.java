e708af1a33f8de9b1a43fd5241b8e8819989b164
BrendonKupsch1 <Brendon.Kupsch1@marist.edu>
2022-03-02 19:22:09 -0500

    hw5 prob 4

--- hw/5/Problem3.java
+++ hw/5/Problem3.java
@@ -5,5 +5,22 @@
 import java.util.Scanner;
 public class Problem3 
 {
-    public static String title(String s
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a phrase to capitalize each word of it: ");
+        String phrase = input.next();
+        System.out.println(title(phrase));
+    }
+    public static String title(String s)
+    {
+        String words[]=s.split("\\s");  
+        String capitalizeWord="";  
+        for(String w:words){  
+            String first=w.substring(0,1);  
+            String afterfirst=w.substring(1);  
+            capitalizeWord+=first.toUpperCase()+afterfirst+" ";  
+        }  
+        return capitalizeWord.trim();  
+    }
 }
--- hw/5/Problem4.java
+++ hw/5/Problem4.java
@@ -3,6 +3,37 @@
 //Prof. Arias
 //HW 5 Problem 4
 import java.util.Scanner;
-public class Problem4 {
-    
+public class Problem4 
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter total seconds: ");
+        long seconds = input.nextLong();
+        System.out.println(format(seconds));
+    }
+    public static String format(long seconds)
+    {
+        long min = (seconds / 60);
+        long hours = (min / 60);
+        long sec = (seconds % 60);
+        min = (min % 60);
+        String sHour = hours + "";
+        if (sHour.length() < 2)
+        {
+            sHour = "0" + sHour;
+        }
+        String sMin = min + "";
+        if (sMin.length() < 2)
+        {
+            sMin = "0" + sMin;
+        }
+        String sSec = sec + "";
+        if (sSec.length() < 2)
+        {
+            sSec = "0" + sSec;
+        }
+        String s = sHour + ":" + sMin + ":" + sSec;
+        return s;
+    }
 }
