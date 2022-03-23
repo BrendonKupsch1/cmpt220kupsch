060491f97311fb4c6e4c3eba83a4cd507b37cc34
BrendonKupsch1 <Brendon.Kupsch1@marist.edu>
2022-03-23 18:09:22 -0400

    hw 6 pr 1

--- hw/6/Problem1.java
+++ hw/6/Problem1.java
@@ -1,4 +1,14 @@
+import java.util.Random;
+
 public class Problem1 
 {
-    
+    public static void main(String[] args)
+    {
+        int counts[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        for (int i = 0; i < 100; i++)
+        {
+            int rnd = new Random().nextInt(counts.length);
+            System.out.println(rnd);
+        }
+    }
 }
