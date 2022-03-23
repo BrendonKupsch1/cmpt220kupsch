181b433c02006749d2827db7e23e639bc2111827
BrendonKupsch1 <Brendon.Kupsch1@marist.edu>
2022-03-23 19:25:20 -0400

    hw 6 prob 2,3

--- hw/6/Problem2.java
+++ hw/6/Problem2.java
@@ -1,4 +1,47 @@
-public class Problem2 
-{
-    
+import java.util.Scanner;
+
+public class Problem2{
+	private double[][] points;
+
+	public Problem2(double[][] points)
+  {
+		this.points = points;
+	}
+
+	public static double[][] getPoints()
+  {
+		System.out.println("Enter amount of points");
+		Scanner s = new Scanner(System.in);
+		int amount = Integer.valueOf(s.nextLine());
+		double[][] points = new double[amount][2];
+		System.out.println("Enter " + amount + " pairs of coordinates (e.g. 1,1) on new lines each");
+		for(int i = 0; i < amount; i++)
+    {
+			String coord = s.nextLine();
+			points[i][0] = Double.valueOf(coord.split(",")[0]);
+			points[i][1] = Double.valueOf(coord.split(",")[1]);
+		}
+		s.close();
+		return points;
+	}
+
+	private double calcArea()
+  {
+		double sum = 0;
+		int j = points.length - 1;
+
+		for(int i = 0; i < points.length; i++)
+    {
+			sum += (points[j][0] + points[i][0]) * (points[j][1] - points[i][1]);
+			j = i;
+		}
+		double result = Math.abs(sum / 2.0);
+		return result;
+	}
+
+	public static void main(String[] args)
+  {
+		Problem2 calc = new Problem2(getPoints());
+		System.out.println("Area: " + calc.calcArea() + " units");
+	}
 }
--- hw/6/Problem3.java
+++ hw/6/Problem3.java
@@ -1,4 +1,24 @@
+import java.util.Scanner;
 public class Problem3 
 {
-    
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String str = input.nextLine();
+        System.out.println("Which digit would you like to see the occurunce of: ");
+        int intDigit = input.nextInt();
+        char charDigit = (char)(intDigit + 48);
+        System.out.println("Digit " + charDigit + " occurs " + countChar(str, charDigit) + " times.");
+    }
+    public static int countChar(String str, char c)
+    {
+    int count = 0;
+    for(int i=0; i < str.length(); i++)
+    {    
+        if(str.charAt(i) == c)
+            count++;
+    }
+    return count;
+    }
 }
