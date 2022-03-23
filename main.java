b433eaf133d3d761f93afd2a2edb904ac2279015
BrendonKupsch1 <Brendon.Kupsch1@marist.edu>
2022-03-23 17:50:56 -0400

    hw 6 format

--- main.java
+++ main.java
@@ -1,8 +1,14 @@
 import java.util.Scanner;
-public class main
+public class main 
 {
-    public static void main(String[] args)
-    {
-        
+    public static void main(String[] args) {
+        int i = 4; int j = 5;
+        // Fill in the code to invoke the printSum method to display the sum of i and j.
+        printSum(i, j);
+    }
+    
+    public static void printSum(int i, int j) {
+        // Fill in the code here to display the sum of i and j.
+        System.out.println(i + j);
     }
 }
