94eb25aefdf192b0cf9056e536e254ce58eae6ec
BrendonKupsch1 <Brendon.Kupsch1@marist.edu>
2022-04-20 19:12:17 -0400

    Hw 9

--- hw/9/Problem2.java
+++ hw/9/Problem2.java
@@ -2,7 +2,24 @@
 //CMPT_220L_202
 //Prof. Arias
 //HW 9 Problem 2
+import java.util.*;
 public class Problem2 
 {
-    
+    public static int bin2Dec(String binaryString) throws NumberFormatException 
+    {
+		int decimal = 0;
+		for (int i = 0, j = binaryString.length() - 1; i < binaryString.length(); i++, j--) 
+            {
+			if (binaryString.charAt(i) < '0' || binaryString.charAt(i) > '1')
+				throw new NumberFormatException("The string is not a binary string");
+			decimal += (Integer.parseInt(String.valueOf(binaryString.charAt(i)))) * Math.pow(2, j);
+		    }
+		return decimal;
+	}
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a binary string to convert into a decimal number: ");
+        System.out.println("The decimal number is " + bin2Dec(input.nextLine()) + ".");
+    }
 }
--- hw/9/Problem3.java
+++ hw/9/Problem3.java
@@ -2,7 +2,54 @@
 //CMPT_220L_202
 //Prof. Arias
 //HW 9 Problem 3
+import java.util.*;
 public class Problem3 
 {
-    
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a binary string to convert into a decimal number: ");
+        System.out.println("The decimal number is " + bin2Dec(input.nextLine()) + ".");
+        System.out.println("Enter a normal string to throw the custom exception: ");
+        System.out.println(bin2Dec(input.nextLine()));
+    }
+    public static int bin2Dec(String binary) throws BinaryFormatException 
+    {
+        if (!isBinary(binary)) 
+        {
+            throw new BinaryFormatException(binary + " is not a binary number.");
+        }
+        int power = 0;
+        int decimal = 0;
+        for (int i = binary.length() - 1; i >= 0; i--) 
+        {
+
+            if (binary.charAt(i) == '1') 
+            {
+                decimal += Math.pow(2, power);
+            }
+            power++;
+        }
+        return decimal;
+    }
+
+    public static boolean isBinary(String binary) 
+    {
+
+        for (char ch : binary.toCharArray()) 
+        {
+            if (ch != '1' && ch != '0') return false;
+        }
+        return true;
+    }
+
+}
+
+class BinaryFormatException extends IllegalArgumentException 
+{
+
+    BinaryFormatException(String s) 
+    {
+        super(s);
+    }
 }
