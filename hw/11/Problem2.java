5d1201b018ec40a9282c05d21716fe8bd74db4bc
BuildTools <unconfigured@null.spigotmc.org>
2022-05-06 12:58:59 -0400

    hw 11 work

--- hw/11/Problem2.java
+++ hw/11/Problem2.java
@@ -3,6 +3,7 @@
 //Prof. Arias
 //HW 11 Problem 2
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Problem2
@@ -27,27 +28,41 @@ public class Problem2
         System.out.println("The maximum weight of items placed in the bag is " + m(numOfItems - 1, weightLimit, w));
         
     }
-
-
-    public static double m(int i, double weightLimit, double[] w)
-    {
-        if(i == 0)
-        {
-            return 0;
+    public static ArrayList<Integer> m(int i, double weightLimit, double[] w){
+        ArrayList<Integer> ReturnArray = new ArrayList<Integer>();
+        return m_helper(i, weightLimit, w, ReturnArray);
+      }
+      
+      public static ArrayList<Integer> m_helper(int i, double weightLimit, double[] w, ArrayList<Integer> ReturnArray){
+         if(i == 0)
+              {
+                  return ReturnArray;
+              }
+              if(weightLimit == 0)
+              {
+                  return ReturnArray;
+              }
+              if(w[i] > weightLimit)
+              {
+                  return m(i - 1, weightLimit, w, ReturnArray);
+              }
+      
+              ArrayList<Integer> copy = new ArrayList<Integer>(ReturnArray);
+              copy.add(w[i]);
+      
+              return Math.max(m(i - 1, weightLimit, w, ReturnArray), m(i, weightLimit - w[i], w, copy));
+      }
+      
+      public static ArrayList<Integer> ArrayListMax (ArrayList<Integer> x, ArrayList<Integer> y){
+        int s1=0, s2=0;
+        for(auto i : x){
+          s1 +=i;
         }
-        if(weightLimit == 0)
-        {
-            return 0;
+        for(auto i : x){
+          s2 +=i;
         }
-        if(w[i] > weightLimit)
-        {
-            return m(i - 1, weightLimit, w);
-        }
-        return Math.max(m(i - 1, weightLimit, w), w[i] + m(i, weightLimit - w[i], w));
-    }
-
-    public static ArrayList<Integer> m(int i, double weightLimit, double[] w)
-    {
-
-    }
+      
+        if(s1 > s2) { return s1; }
+        return s2;
+      }
 }
---
+++ prj/5/Project5.java
@@ -0,0 +1,9 @@
+//Brendon Kupsch
+//CMPT_220L_202
+//Prof. Arias
+//Project 5
+
+public class Project5 
+{
+    
+}
